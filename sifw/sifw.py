848eeec877f30024057e0fa8b0d2c9f6eeab2bca
Angel Beltran <beltranbeaverdam@gmail.com>
2016-02-11 07:15:21 -0700

    added current work

---
+++ sifw/sifw.py
@@ -0,0 +1,131 @@
+from numpy import linspace, zeros
+import matplotlib.pyplot as plt
+import math
+import time
+import datetime
+import sys
+
+def get_parameters():
+    f = open('sifw_parameters.csv', 'rho')
+    mapping = dict()
+    for line in f:
+        data = line.split(',')
+        for i in range(len(data)):
+            data[i] = data[i].strip(' ')
+            data[i] = data[i].strip('\n')
+        for i in range(1, len(data)):
+            if '.' not in data[i]:
+                data[i] = int(data[i])
+            else:
+                data[i] = float(data[i])
+        mapping[data[0]] = data[1:]
+    f.close()
+    return mapping
+
+def sifw(days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, _D):
+    # Computational constants
+    dt = 1./(24)        # intervals of 1 hour (float)
+    # days = 365             # Simulation lasts 365 days
+    N_t = int(days/dt)     # no of intervals in simulation
+    t = linspace(0, days, N_t + 1)
+    S = zeros(N_t + 1)
+    I = zeros(N_t + 1)
+    F = zeros(N_t + 1)
+    W = zeros(N_t + 1)
+
+    # Initial Condition# NOTE: RETURN HERE TO SET THE INITIAL CONDITIONS
+    S[0] = H - 5000
+    I[0] = 5000
+    F[0] = 0
+    W[0] = 1
+
+    def D(t):
+        return _D + _D*math.sin(2*math.pi*t/365)/2
+
+    print H
+    print S[0], I[0], F[0], W[0]
+    # Approximations
+    for i in range(N_t):
+        S[i+1] = S[i] + dt*((b - d)*(H - S[i] - I[i]) + rho*I[i] - beta*S[i]*F[i]/(k*W[i] + F[i]))
+        I[i+1] = I[i] + dt*(beta*S[i]*F[i]/(k*W[i] + F[i]) - (d + rho + alpha)*I[i])
+        F[i+1] = F[i] + dt*((r - mu)*F[i] + _lambda*I[i])
+        W[i+1] = W[i] + dt*(p + s - D(i)*W[i])
+
+        if (i % 100 == 0):
+            print S[i+1], I[i+1], F[i+1], W[i+1]
+
+    return S, I, F, W, t
+
+def display_sir(S, I, F, W, t, days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D):
+    fig = plt.figure()
+    l1, l2, l3, l4 = plt.plot(t, S, t, I, t, F, t, W)
+    fig.legend((l1, l2, l3, l4), ('S', 'I', 'F', 'W'), 'upper left')
+
+    ax = fig.add_subplot(111)
+    ax.set_title('SIFW Model')
+    text = 'days = {0}\nH = {1}\nb = {2}\nd = {3}\nbeta = {4}\nrho = {5}\nk = {6}\nalpha = {7}\nr = {8}\nmu = {9}\nlambda = {10}\np = {11}\ns = {12}\nD = {13}\n'.format(days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+    ax.text(317, 8000, text, style='italic',
+        bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+
+    plt.xlabel('days')
+    plt.show()
+
+def save_sir(S, I, F, W, t, days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D):
+    fig = plt.figure()
+    l1, l2, l3, l4 = plt.plot(t, S, t, I, t, F, t, W)
+    fig.legend((l1, l2, l3, l4), ('S', 'I', 'F', 'W'), 'upper left')
+
+    ax = fig.add_subplot(111)
+    ax.set_title('SIFW Model')
+    text = 'days = {0}\nH = {1}\nb = {2}\nd = {3}\nbeta = {4}\nrho = {5}\nk = {6}\nalpha = {7}\nr = {8}\nmu = {9}\nlambda = {10}\np = {11}\ns = {12}\nD = {13}\n'.format(days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+    ax.text(317, 8000, text, style='italic',
+        bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+    # text_fig.suptitle('bold figure suptitle', fontsize=14, fontweight='bold')
+    plt.xlabel('days')
+
+    print '\ndays = {0}\nH = {1}\nb = {2}\nd = {3}\nbeta = {4}\nrho = {5}\nk = {6}\nalpha = {7}\nr = {8}\nmu = {9}\nlambda = {10}\np = {11}\ns = {12}\nD = {13}'.format(days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+    print ''
+    title = raw_input('Name your pdf: ')
+    title = title.split(' ')[0].split('.')[0]
+    if title == '':
+        ts = time.time()
+        title = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+    plt.savefig(title + '.pdf'); # plt.savefig('tmp.png')
+
+def main():
+    # get data from parameter csv
+    data = get_parameters()
+
+    # get shortest row's length
+    m = len(data['H'])
+    for k in data:
+        m = min(m, len(data[k]))
+
+    for i in range(m):
+        days = data['days'][i]
+        H = data['H'][i]
+        b = data['b'][i]
+        d = data['d'][i]
+        beta = data['beta'][i]
+        rho = data['rho'][i]
+        k = data['k'][i]
+        alpha = data['alpha'][i]
+        r = data['r'][i]
+        mu = data['mu'][i]
+        _lambda = data['_lambda'][i]
+        p = data['p'][i]
+        s = data['s'][i]
+        D = data['D'][i]
+
+        S, I, F, W, t = sifw(days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+        if len(sys.argv) == 1:
+            display_sir(S, I, F, W, t, days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+        elif sys.argv[1] == 'show':
+            display_sir(S, I, F, W, t, days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+        elif sys.argv[1] == 'save':
+            save_sir(S, I, F, W, t, days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+        else:
+            display_sir(S, I, F, W, t, days, H, b, d, beta, rho, k, alpha, r, mu, _lambda, p, s, D)
+
+if __name__ == "__main__":
+    main()
---
+++ sir/sir_contamination.py
@@ -0,0 +1,119 @@
+from numpy import linspace, zeros
+import matplotlib.pyplot as plt
+import math
+import time
+import datetime
+import sys
+
+def get_parameters():
+    f = open('sir_contamination_parameters.csv', 'r')
+    mapping = dict()
+    for line in f:
+        data = line.split(',')
+        for i in range(len(data)):
+            data[i] = data[i].strip(' ')
+            data[i] = data[i].strip('\n')
+        for i in range(1, len(data)):
+            if '.' not in data[i]:
+                data[i] = int(data[i])
+            else:
+                data[i] = float(data[i])
+        mapping[data[0]] = data[1:]
+    f.close()
+    return mapping
+
+def sir(D, H, n, a, K, r, nb_mb, _e):
+    # Computational constants
+    dt = 1./(24)        # intervals of 1 hour
+    # D = 365             # Simulation lasts 365 days
+    N_t = int(D/dt)     # no of intervals in simulation
+    t = linspace(0, N_t*dt, N_t + 1)
+    S = zeros(N_t + 1)
+    I = zeros(N_t + 1)
+    B = zeros(N_t + 1)
+
+    # Initial Condition
+    S[0] = H - 1
+    I[0] = 1
+    B[0] = 0
+
+    def e(t):
+        return _e + _e*math.sin(2*math.pi*t*dt/365)
+
+    # Approximations
+    for i in range(N_t):
+        S[i+1] = S[i] + dt*(n*(H - S[i]) - a*S[i]*B[i]/(K + B[i]))
+        I[i+1] = I[i] + dt*(a*B[i]*S[i]/(K + B[i]) - r*I[i])
+        B[i+1] = B[i] + dt*(B[i]*(nb_mb) + e(i)*I[i])
+
+    return S, I, B, t
+
+
+def display_sir(S, I, B, t, D, H, n, a, K, r, nb_mb, e):
+    fig = plt.figure()
+    l1, l2, l3 = plt.plot(t, S, t, I, t, B)
+    fig.legend((l1, l2, l3), ('S', 'I', 'B'), 'upper left')
+
+    ax = fig.add_subplot(111)
+    ax.set_title('SIR Contamination Model')
+    text = 'D = {0}\nH = {1}\nn = {2}\na = {3}\nK = {4}\nr = {5}\nnb_mb = {6}\ne = {7}\n'.format(D, H, n, a, K, r, nb_mb, e)
+    ax.text(317, 8000, text, style='italic',
+        bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+
+    plt.xlabel('days')
+    plt.show()
+
+def save_sir(S, I, B, t, D, H, n, a, K, r, nb_mb, e):
+    fig = plt.figure()
+    l1, l2, l3 = plt.plot(t, S, t, I, t, B)
+    fig.legend((l1, l2, l3), ('S', 'I', 'B'), 'upper left')
+    # text_fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title('SIR Contamination Model')
+    text = 'D = {0}\nH = {1}\nn = {2}\na = {3}\nK = {4}\nr = {5}\nnb_mb = {6}\ne = {7}\n'.format(D, H, n, a, K, r, nb_mb, e)
+    ax.text(317, 8000, text, style='italic',
+        bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+    # text_fig.suptitle('bold figure suptitle', fontsize=14, fontweight='bold')
+    plt.xlabel('days')
+
+
+    display_string = 'Name your pdf:\nD = {0}\nH = {1}\nn = {2}\na = {3}\nK = {4}\nr = {5}\nnb_mb = {6}\ne = {7}\n'.format(D, H, n, a, K, r, nb_mb, e)
+    title = raw_input(display_string)
+    print ''
+    title = title.split(' ')[0].split('.')[0]
+    if title == '':
+        ts = time.time()
+        title = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+    plt.savefig(title + '.pdf'); # plt.savefig('tmp.png')
+
+def main():
+    # get data from parameter csv
+    data = get_parameters()
+
+    # get longest row's length
+    m = len(data['a'])
+    for k in data:
+        m = min(m, len(data[k]))
+
+    for i in range(m):
+        D = data['D'][i]
+        H = data['H'][i]
+        n = data['n'][i]
+        a = data['a'][i]
+        K = data['K'][i]
+        r = data['r'][i]
+        nb_mb = data['nb_mb'][i]
+        e = data['e'][i]
+
+        S, I, B, t = sir(D, H, n, a, K, r, nb_mb, e)
+        if len(sys.argv) == 1:
+            display_sir(S, I, B, t, D, H, n, a, K, r, nb_mb, e)
+        elif sys.argv[1] == 'show':
+            display_sir(S, I, B, t, D, H, n, a, K, r, nb_mb, e)
+        elif sys.argv[1] == 'save':
+            save_sir(S, I, B, t, D, H, n, a, K, r, nb_mb, e)
+        else:
+            display_sir(S, I, B, t, D, H, n, a, K, r, nb_mb, e)
+
+if __name__ == "__main__":
+    main()
